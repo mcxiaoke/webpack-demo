fceb2ffdeae9cef2f4b503a085632f195ae12137
mcxiaoke <mcxiaoke@live.com>
2021-06-15 11:32:42 +0800

    webpack code splitting a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component(){
     let element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','webpack'],' ');
+    let btn = document.createElement('button');
+    element.innerHTML = _.join(['<p>','Hello','webpack','</p>'],' ');
+    
+    btn.innerHTML = 'Click Here, Check Console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
